981c67a29e6d8d3254c005504718fa0ac1c02480
码元 <wjy849111934@163.com>
2018-10-20 22:46:18 +0800

    新增JMath

---
+++ src/main/java/jtool/JMath.java
@@ -0,0 +1,48 @@
+package jtool;
+
+import java.math.BigInteger;
+
+public class JMath
+{
+    /**
+     * a-b
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public static BigInteger BigIntegerSub (long a, long b)
+    {
+        BigInteger big_a = BigInteger.valueOf(a);
+        BigInteger big_b = BigInteger.valueOf(b);
+        return big_a.subtract(big_b);
+    }
+
+    /**
+     * a+b
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public static BigInteger BigIntegerAdd (long a, long b)
+    {
+        BigInteger big_a = BigInteger.valueOf(a);
+        BigInteger big_b = BigInteger.valueOf(b);
+        return big_a.add(big_b);
+    }
+
+    /**
+     * a*b
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public static BigInteger BigIntegerMultiply (long a, long b)
+    {
+        BigInteger big_a = BigInteger.valueOf(a);
+        BigInteger big_b = BigInteger.valueOf(b);
+        return big_a.multiply(big_b);
+    }
+}
